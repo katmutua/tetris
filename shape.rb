c338f4d4763c0d2a9174e8bfc57375f38ce42da2
katmutua <jkm.mutua@gmail.com>
2015-08-25 16:47:25 +0300

    include shape files

---
+++ shape.rb
@@ -0,0 +1,149 @@
+require 'gosu'
+
+class Shape
+  attr_accessor :rotation
+
+  def initialize(game)
+    @game = game
+    @blocks = []
+    @last_fall_update = Gosu::milliseconds
+    @last_move_update = Gosu::milliseconds
+
+    @blocks = [Block.new(game), Block.new(game), Block.new(game), Block.new(game)]
+
+    @x = @y = 0
+    @falling = true
+
+    @rotation_block = @blocks[1]
+    @rotation_cycle = 1
+    @rotation = 0
+  end
+
+  def apply_rotation
+    if @rotation_block != nil
+      (1..@rotation.modulo(@rotation_cycle)).each do |i|
+        @blocks.each do |block|
+          old_x = block.x
+          old_y = block.y
+          block.x = @rotation_block.x + (@rotation_block.y - old_y)
+          block.y = @rotation_block.y - (@rotation_block.x - old_x)
+        end
+      end
+    end
+  end
+
+  def reverse
+    center = (get_bounds[2] + get_bounds[0]) / 2.0
+    @blocks.each do |block|
+      block.x = 2*center - block.x - @game.block_width
+    end
+  end
+
+  def get_bounds
+    x_min = []
+    y_min = []
+    x_max = []
+    y_max = []
+    @blocks.each do |block|
+      x_min << block.x
+      y_min << block.y
+
+      x_max << block.x + block.width
+      y_max << block.y + block.height
+    end
+
+    return [x_min.min, y_min.min, x_max.max, y_max.max]
+  end
+
+  def get_blocks    
+    @blocks[0].x = @x
+    @blocks[1].x = @x
+    @blocks[2].x = @x
+    @blocks[3].x = @x
+    @blocks[0].y = @y
+    @blocks[1].y = @blocks[0].y + @blocks[0].height
+    @blocks[2].y = @blocks[1].y + @blocks[1].height
+    @blocks[3].y = @blocks[2].y + @blocks[2].height
+  
+    apply_rotation
+  
+  @blocks.each { |block| block.color = 0xffb2ffff }
+  end
+
+  def needs_fall_update?
+    if (@game.button_down?(Gosu::KbDown))
+      updateInterval = 100
+    else
+      updateInterval = 500 - @game.level*50
+    end
+    if (Gosu::milliseconds - @last_fall_update > updateInterval)
+      @last_fall_update = Gosu::milliseconds
+    end
+  end
+
+  def needs_move_update?
+    if (Gosu::milliseconds - @last_move_update > 100)
+      @last_move_update = Gosu::milliseconds
+    end
+  end
+
+  def draw
+    get_blocks.each { |block| block.draw }
+  end
+
+  def update
+    if (@falling)
+      old_x = @x
+      old_y = @y
+
+      if needs_fall_update?
+        @y = (@y + @game.block_height)
+      end
+
+      if (collide)
+        @y = (old_y)
+        @falling = false
+        @game.spawn_next_shape
+        @game.delete_lines_of(self)
+      else
+        if needs_move_update?
+          if (@game.button_down?(Gosu::KbLeft))
+            @x = (@x - @game.block_width)
+          end
+          if (@game.button_down?(Gosu::KbRight))
+            @x = (@x + @game.block_width)
+          end
+
+          if (collide)
+            @x = (old_x)
+          end
+        end
+      end
+    end
+  end
+
+  def collide
+    get_blocks.each do |block|
+      collision = block.collide_with_other_blocks;
+      if (collision)
+        return true
+      end
+    end
+
+    bounds = get_bounds
+
+    if (bounds[3] > @game.height)
+      return true
+    end
+
+    if (bounds[2] > @game.width)
+      return true
+    end
+
+    if (bounds[0] < 0)
+      return true
+    end
+    return false
+  end
+
+end
---
+++ tetris.rb
@@ -0,0 +1,134 @@
+[
+  'rubygems',
+  'gosu',
+].each(&method(:require))
+
+[
+  'block',
+  'shape',
+  'shape_cube',
+  'shape_i', 
+  'shape_j',
+  'shape_l', 
+  'shape_s', 
+  'shape_t', 
+  'shape_z'
+].each{ |file| require_relative File.expand_path("./lib/#{file}.rb") }
+
+class TetrisGameWindow < Gosu::Window
+  attr_accessor :blocks
+  attr_reader :block_height, :block_width
+  attr_reader :level
+  attr_reader :falling_shape
+
+  STATE_PLAY = 1
+  STATE_GAMEOVER = 2
+  DEFAULT_VALUE = 0
+  MAX_LINES = 10
+
+  def initialize
+    super(320, 640, false)
+
+    @block_width = 32
+    @block_height = 32
+    @blocks = []
+    @state = STATE_PLAY
+    spawn_next_shape
+    @song = Gosu::Song.new("media/bongz.mp3")
+    @lines_cleared = DEFAULT_VALUE
+    @level = DEFAULT_VALUE
+    self.caption = "Tetris : #{@lines_cleared} lines"
+  end
+
+  def update
+    if (@state == STATE_PLAY)
+      if (@falling_shape.collide)
+        @state = STATE_GAMEOVER
+      else
+        @falling_shape.update
+      end
+
+      @level = @lines_cleared / 10
+      self.caption = "Tetris : #{@lines_cleared} lines"
+    else
+      if (button_down?(Gosu::KbSpace))
+        @blocks = []
+        @falling_shape = nil
+        @level = 0
+        @lines_cleared = 0
+        spawn_next_shape
+
+        @state = STATE_PLAY
+      end
+    end
+
+    if (button_down?(Gosu::KbEscape))
+      close
+    end
+    @song.play(true)
+  end
+
+  def draw
+    @blocks.each { |block| block.draw }
+    @falling_shape.draw
+
+    if @state == STATE_GAMEOVER
+      text = Gosu::Image.from_text(self, "Game Over", "Arial", 40)
+      text.draw(width/2 - 90, height/2 - 20, 0, 1, 1)
+    end
+  end
+
+  def button_down(id)
+    if (id == Gosu::KbSpace && @falling_shape != nil)
+      @falling_shape.rotation += 1
+      if (@falling_shape.collide)
+        @falling_shape.rotation -= 1
+      end
+    end
+  end
+
+  def spawn_next_shape
+    if (@falling_shape != nil)
+      @blocks += @falling_shape.get_blocks
+    end
+
+    generator = Random.new
+    shapes = generate_shapes
+    shape = generator.rand(0..(shapes.length-1))
+    @falling_shape = shapes[shape]
+  end
+
+  def generate_shapes
+    shapes = []
+    shape_types = ['Cube', 'I', 'J', 'L', 'T','S','Z']
+    shape_types.each { |shape| shapes << Kernel.const_get("Shape#{shape}").new(self) }
+    shapes
+  end
+
+  def line_complete(y)
+    i = @blocks.count { |item| item.y == y }
+    if (i == width / block_width)
+      return true;
+    end
+    return false;
+  end
+
+  def delete_lines_of(shape)
+    deleted_lines = []
+    if (line_complete(14))
+ 				deleted_lines.push(14)
+      @blocks = @blocks.delete_if { |item| item.y == block.y }
+    end
+     @lines_cleared += deleted_lines.length
+
+  	@blocks.each do |block|
+    	i = deleted_lines.count { |y| y > block.y }
+    	block.y += i*block_height
+  	end
+  end
+end
+
+if (!$testing)
+  window = TetrisGameWindow.new
+  window.show
+end
